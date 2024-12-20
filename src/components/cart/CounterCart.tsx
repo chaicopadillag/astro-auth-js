37324fae77201d242f90084c89cb7e673ec6c95b
chaicopadillag <xt9738@rimac.com.pe>
2024-12-20 16:56:13 -0500

    feat: integrate Swiper for product slideshow, add cart counter component, and enhance product page interactivity

---
+++ src/components/cart/CounterCart.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const CounterCart = () => {
+  const [counter, setCounter] = useState(5);
+
+  return (
+    <a href='/cart' className='relative inline-block'>
+      <span className='absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5'>{counter}</span>
+
+      <svg xmlns='http://www.w3.org/2000/svg' width={32} height={32} viewBox='0 0 32 32'>
+        <g fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth={2}>
+          <path d='M6 6h24l-3 13H9m18 4H10L5 2H2'></path>
+          <circle cx={25} cy={27} r={2}></circle>
+          <circle cx={12} cy={27} r={2}></circle>
+        </g>
+      </svg>
+    </a>
+  );
+};
--- src/components/products/ProductSlideshow.tsx
+++
@@ -1,9 +0,0 @@
-import { type FC } from 'react';
-
-type Props = {
-  images: string[];
-};
-
-export const ProductSlideshow: FC<Props> = ({ images }) => {
-  return <div>ProductSlideshow</div>;
-};
